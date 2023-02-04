c238b8d7b1ab5d42f462db6b65026357a0a1e25f
shahidmonowarr <shahidmonowar21@gmail.com>
2023-02-05 01:29:10 +0600

    contact form

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Contact from "../../components/Contact/Contact";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 
@@ -6,6 +7,7 @@ const Home = () => {
   return (
     <div>
       <Navbar />
+      <Contact />
       <Footer />
     </div>
   );
